b7b842a85f7b5a9167babff1188ad016d7301be4
Diego <d.garciabrisa@gmail.com>
2021-02-14 19:50:30 +0100

    movie searcher added

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let url = 'http://api.themoviedb.org/3/movie/popular?api_key=c0b6dea31a9d647a6b7
 
 // 500 pages in total
 
-for(let i = 1; i<=10; i++) {
+for(let i = 1; i<=2; i++) {
     let page = '&page=';
     let promise = axios.get(url+page+i);
 
@@ -32,32 +32,54 @@ for(let i = 1; i<=10; i++) {
     
         });
     }
-
+    
+    //Esta funcion recibe un array de peliculas y las pinta
     pelisArrayPromise.then(pintaLasPelis);
 }
 
 
 
 
-// let search = prompt('Search for a movie or series')
-// let searchApi = axios.get('http://api.themoviedb.org/3/search/multi?api_key=c0b6dea31a9d647a6b7d1eafa59bacaa&query='+search)
-//Movie searcher
+// Movie searcher
+
+let urlSearcher = 'http://api.themoviedb.org/3/search/multi?api_key=c0b6dea31a9d647a6b7d1eafa59bacaa&query=';
 
 
 
+
+document.querySelector('#submit').addEventListener('click', () =>{
+    let input = document.querySelector('#searcher').value;
+    
+    let searchApi = axios.get(urlSearcher+input);
+    let arraySearch = searchApi.then((response) => response.data.results)
     
 
+    const searchRender = (pelisCollection) => { 
+        const divPelisDomElement = document.getElementById('showSelection');
+        
+        pelisCollection.map((pelicula) =>{
+            const pathImg = 'https://image.tmdb.org/t/p/w500';
+            const newPeliDomElement = document.createElement('div');
+            const titleMovie = document.createElement('div')
+            const moviePic = document.createElement('img')
+            
+            divPelisDomElement.appendChild(newPeliDomElement)
+            newPeliDomElement.appendChild(titleMovie)
+            newPeliDomElement.appendChild(moviePic)
+            
+            newPeliDomElement.setAttribute('class', 'titlePicture')
+            titleMovie.setAttribute('class', 'title')
+            titleMovie.innerHTML = pelicula.title;
+            moviePic.setAttribute('src', pathImg+pelicula.poster_path);
+    
+        });
+    }
+    
+    //Esta funcion recibe un array de peliculas y las pinta
+    arraySearch.then(searchRender);
 
+});
 
-
-
-
-// &page=numero
-
-
-
-
-//Esta funcion recibe un array de peliculas y pinta en un div una linea por cada pelicula
 
 
 
